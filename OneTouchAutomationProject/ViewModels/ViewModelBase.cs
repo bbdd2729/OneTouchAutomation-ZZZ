b8668817d06f0e32922414d6a9863714d9715b9b
bbdd <love4yuihirasawa@gmail.com>
2026-06-19 18:01:25 +0800

    :tada:

---
+++ OneTouchAutomationProject/ViewModels/ViewModelBase.cs
@@ -0,0 +1,5 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace OneTouchAutomationProject.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject { }
---
+++ OneTouchAutomationProject/Views/MainWindow.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace OneTouchAutomationProject.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow() { InitializeComponent(); }
+}
